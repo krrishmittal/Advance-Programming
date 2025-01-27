eb69626e53e338968d8bf244c6161b85d4db9857
krrishmittal <mittalkrrish0811@gmail.com>
2025-01-27 10:25:19 +0530

    Comments added

--- leetcode/Contains Duplicate.cpp
+++ leetcode/Contains Duplicate.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        //check for the next element if its same or not
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]){
                 return true;
